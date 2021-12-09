2e662b21d2543c5230935f48ec27b7c8e61c0b63
cchileshe <chi06001@byui.edu>
2021-12-09 18:03:50 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-                  
+import * as workouts from "../../model/data.js";             
 //=============================
 // CURRENT DATE
 //=============================
@@ -8,110 +8,31 @@
 
 
 //=============================
-//  WORKOUT LIST FILTERS
+// SIDE BAR
 //=============================
-function displayWorkouts() {
-    alert(weeks)
-}
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "170px";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
 
-// $( document ).ready(function() {
-//     alert("one")
-//     "use strict";
-    
-//     var todo = function() { 
-//         $('.todo-list .todo-item input').click(function() {
-//         if($(this).is(':checked')) {
-//             $(this).parent().parent().parent().toggleClass('complete');
-//         } else {
-//             $(this).parent().parent().parent().toggleClass('complete');
-//         }
-//     });
-    
-//     $('.todo-nav .all-task').click(function() {
-//         alert('ok')
-//         // $('.todo-list').removeClass('only-active');
-//         // $('.todo-list').removeClass('only-complete');
-//         // $('.todo-nav li.active').removeClass('active');
-//         // $(this).addClass('active');
-        
-//     });
-    
-//     $('.todo-nav .active-task').click(function() {
-//         $('.todo-list').removeClass('only-complete');
-//         $('.todo-list').addClass('only-active');
-//         $('.todo-nav li.active').removeClass('active');
-//         $(this).addClass('active');
-//     });
-    
-//     $('.todo-nav .completed-task').click(function() {
-//         $('.todo-list').removeClass('only-active');
-//         $('.todo-list').addClass('only-complete');
-//         $('.todo-nav li.active').removeClass('active');
-//         $(this).addClass('active');
-//     });
-    
-//     $('#uniform-all-complete input').click(function() {
-//         if($(this).is(':checked')) {
-//             $('.todo-item .checker span:not(.checked) input').click();
-//         } else {
-//             $('.todo-item .checker span.checked input').click();
-//         }
-//     });
-    
-//     $('.remove-todo-item').click(function() {
-//         $(this).parent().remove();
-//     });
-//     };
-    
-//     todo();
-    
-//     $(".add-task").keypress(function (e) {
-//         if ((e.which == 13)&&(!$(this).val().length == 0)) {
-//             $('<div class="todo-item"><div class="checker"><span class=""><input type="checkbox"></span></div> <span>' + $(this).val() + '</span> <a href="javascript:void(0);" class="float-right remove-todo-item"><i class="icon-close"></i></a></div>').insertAfter('.todo-list .todo-item:last-child');
-//             $(this).val('');
-//         } else if(e.which == 13) {
-//             alert('Please enter new task');
-//         }
-//         $(document).on('.todo-list .todo-item.added input').click(function() {
-//             if($(this).is(':checked')) {
-//                 $(this).parent().parent().parent().toggleClass('complete');
-//             } else {
-//                 $(this).parent().parent().parent().toggleClass('complete');
-//             }
-//         });
-//         $('.todo-list .todo-item.added .remove-todo-item').click(function() {
-//             $(this).parent().remove();
-//         });
-//     });
-// });
+  $(function() {
+    $(".toggle-menu").click(function() {
+      $(this).toggleClass("active");
+      $('.menu-drawer').toggleClass("open");
+    });
+  });
+
+//=============================
+// LOCAL STORAGE WORKOUTS
+//=============================
+console.log(workouts.weeks);
 
 
-// // side bar
-// $(document).ready(function () {
-//     var trigger = $('.hamburger'),
-//         overlay = $('.overlay'),
-//        isClosed = false;
-  
-//       trigger.click(function () {
-//         hamburger_cross();      
-//       });
-  
-//       function hamburger_cross() {
-  
-//         if (isClosed == true) {          
-//           overlay.hide();
-//           trigger.removeClass('is-open');
-//           trigger.addClass('is-closed');
-//           isClosed = false;
-//         } else {   
-//           overlay.show();
-//           trigger.removeClass('is-closed');
-//           trigger.addClass('is-open');
-//           isClosed = true;
-//         }
-//     }
-    
-//     $('[data-toggle="offcanvas"]').click(function () {
-//           $('#wrapper').toggleClass('toggled');
-//     });  
-//   });
+// localStorage.setItem();
